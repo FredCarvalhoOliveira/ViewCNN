a6c83612c166c41fe321116bfd723c71c0bcb5d1
maximus <diogo.freire.dandrade@gmail.com>
2021-01-02 21:24:39 +0000

    created filterviewer and initial template

--- viewCNN/settings.py
+++ viewCNN/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'filterviewer.apps.FilterviewerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
