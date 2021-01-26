1f38f3c89132323652e7ad2cc77c09ab6c8bbaee
FredCarvalhoOliveira <fred.carvalho.oliveira97@gmail.com>
2021-01-26 04:07:36 +0000

    Feature Map export

--- filterviewer/ConvNet/classifier.py
+++ filterviewer/ConvNet/classifier.py
@@ -2,22 +2,17 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# from ConvLayerViewer import ConvLayerViewer
 import matplotlib.pyplot as plt
 
 class ConvNet(nn.Module):
    def __init__(self):
       super().__init__()
-      # self.__layerViewer = ConvLayerViewer()
       self.__isInDebugMode = False
+      self.__featureMaps   = []
 
       self.conv1 = nn.Conv2d(1, 32, 5)
       self.conv2 = nn.Conv2d(32, 64, 5)
       self.conv3 = nn.Conv2d(64, 128, 5)
-
-      # self.conv1 = nn.Conv2d(1,  32,  5)
-      # self.conv2 = nn.Conv2d(32, 64,  5)
-      # self.conv3 = nn.Conv2d(64, 128, 5)
 
       x = torch.randn(100, 100).view(-1, 1, 100, 100)
       self.__to_linear = None
@@ -27,23 +22,17 @@ class ConvNet(nn.Module):
       self.fc2 = nn.Linear(512, 2)
 
    def convs(self, x):
-      layerResp = self.conv1(x)
-      # if self.__isInDebugMode:
-      #    self.__layerViewer.saveConvLayerResponse(layerResp)
-      x = F.max_pool2d(F.relu(layerResp), (2, 2))
+      layerResp1 = self.conv1(x)
+      x = F.max_pool2d(F.relu(layerResp1), (2, 2))
+      layerResp2 = self.conv2(x)
+      x = F.max_pool2d(F.relu(layerResp2), (2, 2))
+      layerResp3 = self.conv3(x)
+      x = F.max_pool2d(F.relu(layerResp3), (2, 2))
 
-      layerResp = self.conv2(x)
-      # if self.__isInDebugMode:
-      #    self.__layerViewer.saveConvLayerResponse(layerResp)
-      x = F.max_pool2d(F.relu(layerResp), (2, 2))
-
-      layerResp = self.conv3(x)
-      # if self.__isInDebugMode:
-      #    self.__layerViewer.saveConvLayerResponse(layerResp)
-      x = F.max_pool2d(F.relu(layerResp), (2, 2))
-
-      # if self.__isInDebugMode:
-      #    self.__layerViewer.writeToFiles()
+      if self.__isInDebugMode:
+         self.saveConvLayerResponse(layerResp1)
+         self.saveConvLayerResponse(layerResp2)
+         self.saveConvLayerResponse(layerResp3)
 
       if self.__to_linear is None:
          self.__to_linear = x[0].shape[0] * x[0].shape[1] * x[0].shape[2]
@@ -56,11 +45,15 @@ class ConvNet(nn.Module):
       x = self.fc2(x)
       return F.softmax(x, dim=1)
 
+   def saveConvLayerResponse(self, layerResponse):
+      layerResponse = layerResponse[0]  # Only one channel
+      self.__featureMaps.append(layerResponse.cpu().detach().numpy())
+
+   def getFeatureMaps(self):
+      return self.__featureMaps
+
    def setDebugMode(self, isInDebugMode):
-      print("Debug " + str(isInDebugMode))
       self.__isInDebugMode = isInDebugMode
-
-
 
 if __name__ == "__main__":
    net = ConvNet()
--- filterviewer/views.py
+++ filterviewer/views.py
@@ -1,16 +1,24 @@
-# import sys
-# sys.path.append(0, './ConvNet')
-#
-# # import .ConvNet.classifier as cls
-
-
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 from django.template import loader
 
+from .ConvNet.classifier import ConvNet
+import torch
 
+################################
+### On server start load CNN ###
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    print("Running on GPU - " + torch.cuda.get_device_name())
+else:
+    device = torch.device("cpu")
+    print("Running on CPU")
+net = ConvNet()
+net.load_state_dict(torch.load("filterviewer/ConvNet/models/modelEpoch_19"))
+net.eval()
+net.to(device)
 
 
 
@@ -18,7 +26,6 @@ def index(request):
     template = loader.get_template('filterviewer/main.html')
     context = {}
     return HttpResponse(template.render(context, request))
-
 
 def layer(request, layer_id):
     from pathlib import Path
@@ -28,9 +35,7 @@ def layer(request, layer_id):
     p = Path("assets", "layer" + str(layer_id) + ".json")
     with p.open() as f:
         data = json.load(f)
-
     return JsonResponse(data, safe=False)
-
 
 def classify(request):
     template = loader.get_template('classify/classify.html')
@@ -40,20 +45,8 @@ def classify(request):
 @csrf_exempt
 def endPt_classify(request):
     from .ConvNet.utils import url_to_image
-    import matplotlib.pyplot as plt
-    import cv2
-    import numpy as np
     import torch
-    import json
-    from .ConvNet.classifier import ConvNet
-
-    if torch.cuda.is_available():
-        device = torch.device("cuda:0")
-        print("Running on GPU - " + torch.cuda.get_device_name())
-    else:
-        device = torch.device("cpu")
-        print("Running on CPU")
-
+    import cv2
 
     IMG_SIZE = 100
     imgUrl   = request.POST['imgUrl']
@@ -65,11 +58,6 @@ def endPt_classify(request):
     imgTensor = imgTensor.float()
     imgTensor = imgTensor.view(-1, 1, IMG_SIZE, IMG_SIZE).to(device)
 
-    net = ConvNet()
-    net.load_state_dict(torch.load("filterviewer/ConvNet/models/modelEpoch_19"))
-    net.eval()
-    net.to(device)
-
     out = net(imgTensor)
     print("Conv output = " + str(out))
 
@@ -77,7 +65,7 @@ def endPt_classify(request):
         data = {"class": 'cat'}
     else:
         data = {"class": 'dog'}
-    print(data["class"])
+    print(">> ServerResponded: " + data["class"])
 
     return JsonResponse(data, safe=False)
 
